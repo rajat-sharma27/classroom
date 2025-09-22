542a60b50ea30c4d503c6f38b78cc9fe58aea0cd
rajat-sharma27 <rajatpndt2703@gmail.com>
2025-09-22 12:45:30 +0530

    practice probelems

---
+++ Assisted problems/KilometerToMiles.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class KilometerToMiles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter distance in kilometers: ");
+        double km = sc.nextDouble();
+        double miles = km * 0.621371;
+        System.out.println("Distance in miles: " + miles);
+        sc.close();
+    }
+}
